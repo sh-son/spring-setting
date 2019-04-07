ef08de97231a3917b408535010ec3836437a2669
pg-sh <tjdgus4722@naver.com>
2019-04-07 22:34:59 +0900

    springMVC controller test

---
+++ src/main/java/com/son/mvcboard/tutorial/controller/JasonController.java
@@ -0,0 +1,19 @@
+package com.son.mvcboard.tutorial.controller;
+
+import com.son.mvcboard.tutorial.domain.ProductVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class JasonController {
+
+    @RequestMapping("/doJson")
+    @ResponseBody
+    public ProductVO doJson() {
+
+        ProductVO productVO = new ProductVO("laptop", 3000000);
+
+        return productVO;
+    }
+}
